7380be39219e133aa3c2d145e74d9609f95df762
Evhenyi Triukhan <evhenyi.triukhan@idapgroup.com>
2019-08-25 14:21:03 +0300

    MainView -> added behavior logic implementation.

--- EightBall/Sources/UI/MainViewController/MainView.swift
+++ EightBall/Sources/UI/MainViewController/MainView.swift
@@ -16,15 +16,39 @@ class MainView: UIView {
     @IBOutlet var answerLabel: UILabel?
     @IBOutlet var tooltipLabel: UILabel?
     
+    private var isStarted = false
+    
+    // MARK: - View Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.invitationLabel?.text = "Shake iPhone to get answer!"
+        [self.answerLabel, self.tooltipLabel]
+            .forEach { $0?.text = "" }
+    }
+    
     // MARK: - Public API
     
     func show(answer: AnswerModel) {
         DispatchQueue.main.async { [weak self] in
+            if self?.isStarted == false {
+                self?.updateUI()
+            }
+            
             self?.answerLabel?.text = answer.answer
         }
     }
     
     func getQuestion() -> String? {
         return String.random()
+    }
+    
+    // MARK: - Private API
+    
+    private func updateUI() {
+        self.invitationLabel?.removeFromSuperview()
+        self.tooltipLabel?.text = "Shake again if you want more answers!"
+        self.isStarted = true
     }
 }
